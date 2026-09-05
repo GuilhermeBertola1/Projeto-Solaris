d69be00679c959bd944c5a456c428e99aff93432
Guilherme Bertola <guilherme.bertola.123@gmail.com>
2026-09-05 14:38:35 -0300

    fix:Nomenclature about variables

--- js/viewmodels/catalogoViewModel.js
+++ js/viewmodels/catalogoViewModel.js
@@ -1,31 +1,32 @@
 import {obterPlantas} from '../models/plantasModel.js';
 
-export function makeViewModel(onStateChange){
-  const estate = {
+export function makeViewModel(onStateChange) {
+  const estadoBase = {
     termoBusca: '',
     plantas: [],
     carregando: false
-  }
+  };
 
-  const stateAct = new Proxy(estate, {
-    set(target, propriedades, valor) {
-      target[propriedades] = valor;
-      onStateChange(stateAct);
+  const estado = new Proxy(estadoBase, {
+    set(alvo, propriedade, valor) {
+      alvo[propriedade] = valor;
+      onStateChange(estado);
       return true;
     }
   });
 
-  return{
-    estado: stateAct,
-    
-    async buscar(termo){
-      stateAct.carregando = true;
-      stateAct.termoBusca = termo;
+  return {
+    estado,
+
+    async buscar(termo) {
+      estado.carregando = true;
+      estado.termoBusca = termo;
 
       const resultados = await obterPlantas(termo);
 
-      stateAct.plantas = resultados;
-      stateAct.carregando = false;
+      estado.plantas = resultados;
+      estado.carregando = false;
     }
   };
 }
+
