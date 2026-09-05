0fa6f4d5081577b51e354b8625f629e2bc19d406
Guilherme Bertola <guilherme.bertola.123@gmail.com>
2026-09-05 15:12:24 -0300

    Create: Jardim View Model

---
+++ js/viewmodels/jardimViewModel.js
@@ -0,0 +1,60 @@
+import {obterJardim, regarPlanta, obterPlantaDoJardim} from '../models/jardimModel.js'
+import { calcularEstadoPlanta } from '../models/simulacaoModel.js';
+
+export function makeJardimViewModel (onStateChange){
+    const estadoBase = {plantas: []};
+
+    const estado = new Proxy(estadoBase, {
+        set(alvo, propriedade, valor) {
+            alvo[propriedade] = valor;
+            onStateChange(estado);
+            return true;
+        }
+    });
+
+    function carregar(){
+        estado.plantas = obterJardim().map(planta => ({
+            ...planta,
+            ...calcularEstadoPlanta(planta)
+        }));
+    }
+
+    function regar(instanceId){
+        regarPlanta(instanceId);
+        carregar();
+    }
+
+    carregar();
+
+    return {estado,carregar,regar};
+}
+
+export function makePerfilViewModel(instanceId, onStateChange){
+    const estadoBase = {planta: null, encontrada: true};
+
+    const estado = new Proxy(estadoBase, {
+        set(alvo, propriedade, valor) {
+        alvo[propriedade] = valor;
+        onStateChange(estado);
+        return true;
+        }
+    });
+
+    function carregar() {
+        const planta = obterPlantaDoJardim(instanceId);
+        if (!planta) {
+        estado.encontrada = false;
+        return;
+        }
+        estado.planta = { ...planta, ...calcularEstadoPlanta(planta) };
+    }
+
+    function regar() {
+        regarPlanta(instanceId);
+        carregar();
+    }
+
+    carregar();
+
+    return{estado,regar};
+}
